d6d8b8f85c07b411c63f8e76e2a51e33aa21a3a8
Caroline Marsden <car.marsden@gmail.com>
2019-09-27 10:53:50 -0400

    Add price modifier display

--- src/components/NewParty/NewParty.js
+++ src/components/NewParty/NewParty.js
@@ -2,18 +2,74 @@ import React from 'react';
 
 class NewParty extends React.Component {
     state = {
-        partyname: '',
-        location: '',
-        reputation: '',
-        partynotes: '',
-        achievements: '',
+        formFields: {
+            party_name: '',
+            location: '',
+            reputation: '',
+            price_mod: 0,
+            party_notes: '',
+            achievements: '',    
+        },
+        formErrors: {
+            reputation: '',
+        }
     };
+
+    updatePriceModifier(rep) {
+        rep = Number.parseInt(rep, 10);
+        // price modifier is positive for reputations below -2
+        const modIsPositive = (rep < -2) ? true : false;
+
+        // price modifier is reputation / 4 and rounded. e.g. 2.75 - 3.5 becomes 3
+        const base = Math.abs(rep / 4);
+        let priceMod;
+        // numbers up to x.5 get rounded down, above x.5 get rounded up
+        if (base % 1 <= 0.5) {
+            priceMod = Math.floor(base);
+        } else {
+            priceMod = Math.ceil(base)
+        }
+
+        // correct the sign if needed
+        priceMod = modIsPositive ? `+${priceMod}` : -priceMod;
+
+        this.setState({
+            formFields: {
+                ...this.state.formFields,
+                price_mod: priceMod
+            }
+        })
+    }
 
     handleInputChange = e => {
         const field = e.target.name;
-        const value = e.target.value;
+        let value = e.target.value;
+
         this.setState({
-            [field]: value
+            formFields: {
+                ...this.state.formFields,
+                [field]: value
+            }
+        }, () => {this.validateInputs(field, value)})
+    }
+
+    validateInputs(field, value) {
+        const newErrors = {...this.state.formErrors};
+        // validate for reputation
+        if (field === 'reputation') {
+            // convert to number to check
+            value = Number(value);
+            // must be integer -20 to +20
+            if (!Number.isInteger(value) || value < -20 || value > 20) {
+                newErrors.reputation = 'Reputation must be an integer from -20 to +20'
+            } else {
+                newErrors.reputation = '';
+                this.updatePriceModifier(value);
+            }
+        }
+
+        this.setState({
+            formErrors: newErrors
         })
     }
 
@@ -22,7 +78,17 @@ class NewParty extends React.Component {
     }
 
     handleReset = e => {
-        Object.keys(this.state).map((key, idx) => this.setState({ [key]: '' }))
+        const blankForm = {
+            party_name: '',
+            location: '',
+            reputation: '',
+            price_mod: 0,
+            party_notes: '',
+            achievements: '',    
+        }
+        this.setState({
+            formFields: blankForm
+        })
     }
 
     handleSubmit = e => {
@@ -43,10 +109,10 @@ class NewParty extends React.Component {
                             <input 
                                 placeholder='Gloomhaven Gangsters' 
                                 type='text' 
-                                name='partyname'
-                                id='partyname' 
+                                name='party_name'
+                                id='party_name' 
                                 onChange={e => this.handleInputChange(e)}
-                                value={this.state.partyname}
+                                value={this.state.formFields.party_name}
                                 required 
                             />
                         </label>
@@ -57,7 +123,7 @@ class NewParty extends React.Component {
                                 name='location' 
                                 id='location' 
                                 onChange={e => this.handleInputChange(e)}
-                                value={this.state.location}
+                                value={this.state.formFields.location}
                             />
                         </label>
                         <label>Reputation*: 
@@ -69,22 +135,24 @@ class NewParty extends React.Component {
                                 name='reputation' 
                                 id='reputation' 
                                 onChange={e => this.handleInputChange(e)}
-                                value={this.state.reputation}
+                                value={this.state.formFields.reputation}
                                 required 
                             />
                         </label>
+                        <span>{this.state.formErrors.reputation}</span>
+                        <p>Shop Price Modifier: {this.state.formFields.price_mod}</p>
                     </fieldset>
 
                     <fieldset>
                         <legend>Notes</legend>
                         <label>Notes: 
                             <textarea 
-                                name='partynotes' 
-                                id='partynotes' 
+                                name='party_notes' 
+                                id='party_notes' 
                                 rows='5' 
                                 cols='50'
                                 onChange={e => this.handleInputChange(e)}
-                                value={this.state.partynotes}
+                                value={this.state.formFields.party_notes}
                             ></textarea>
                         </label>
                         <label>Achievements: 
@@ -94,7 +162,7 @@ class NewParty extends React.Component {
                                 rows='5' 
                                 cols='50'
                                 onChange={e => this.handleInputChange(e)}
-                                value={this.state.achievements}
+                                value={this.state.formFields.achievements}
                             ></textarea>
                         </label>
                     </fieldset>
