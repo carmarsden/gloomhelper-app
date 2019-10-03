e4b0c4f27c4455c5612246d603000521b0c94fc5
Caroline Marsden <car.marsden@gmail.com>
2019-10-03 17:23:39 -0400

    Use controlled components and form validation

--- src/components/AcctModals/CreateAccount.js
+++ src/components/AcctModals/CreateAccount.js
@@ -2,6 +2,87 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class CreateAccount extends React.Component {
+    state = {
+        username: '',
+        password: '',
+        confirmPassword: '',
+        formErrors: {
+            username: '',
+            password: '',
+            confirmPassword: '',
+            usernameValid: false,
+            passwordValid: false,
+            confirmPasswordValid: false,
+        },
+        formValid: false,
+    }
+
+    handleInputChange = e => {
+        const field = e.target.name;
+        let value = e.target.value;
+
+        this.setState({
+            [field]: value
+        }, () => {this.validateInputs(field, value)})
+    }
+
+    validateInputs(field, value) {
+        const newErrors = this.state.formErrors;
+        const VALID_PW_REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!_@#$%^&])[\S]+/;
+
+        // validate for username
+        if (field === 'username') {
+            if (value.length === 0) {
+                newErrors.username = 'Username is required';
+                newErrors.usernameValid = false;
+            } else {
+                newErrors.username = '';
+                newErrors.usernameValid = true;
+            }
+        }
+
+        // validate for password
+        if (field === 'password') {
+            if (value.length < 8) {
+                newErrors.password = 'Password must be at least 8 characters';
+                newErrors.passwordValid = false;
+            } else if (value.length > 72) {
+                newErrors.password = 'Password must be no more than 72 characters';
+                newErrors.passwordValid = false;
+            } else if (value.startsWith(' ') || value.endsWith(' ')) {
+                newErrors.password = 'Password cannot start or end with empty spaces';
+                newErrors.passwordValid = false;
+            } else if (!VALID_PW_REGEX.test(value)) {
+                newErrors.password = 'Password must contain at least one each of: upper case, lower case, number, and special character';
+                newErrors.passwordValid = false;
+            } else {
+                newErrors.password = '';
+                newErrors.passwordValid = true;
+            }
+        }
+
+        // validate for password confirmation
+        if (field === 'confirmPassword') {
+            if (value !== this.state.password) {
+                newErrors.confirmPassword = 'Must match password';
+                newErrors.confirmPasswordValid = false;
+            } else {
+                newErrors.confirmPassword = '';
+                newErrors.confirmPasswordValid = true;
+            }
+        }
+
+        this.setState({
+            formErrors: newErrors
+        }, this.validateForm)
+    }
+
+    validateForm() {
+        this.setState({
+            formValid: this.state.formErrors.usernameValid && this.state.formErrors.passwordValid && this.state.formErrors.confirmPasswordValid,
+        })
+    }
+
     render() {
         return (
             <form className='account-form' onSubmit={this.props.handleCreateAccount}>
@@ -13,30 +94,39 @@ class CreateAccount extends React.Component {
                             type='text' 
                             name='username' 
                             id='username' 
+                            onChange={e => this.handleInputChange(e)}
+                            value={this.state.username}
                             required 
-                        />
+                        />                        
                     </label>
+                    <span>{this.state.formErrors.username}</span>
                     <label>Password
                         <input 
                             placeholder='password' 
                             type="password" 
                             name='password' 
                             id='password' 
+                            onChange={e => this.handleInputChange(e)}
+                            value={this.state.password}
                             required 
                         />
                     </label>
+                    <span>{this.state.formErrors.password}</span>
                     <label>Confirm Password
                         <input 
                             placeholder='confirm password' 
                             type="password" 
-                            name='confirmpassword' 
-                            id='confirmpassword' 
+                            name='confirmPassword' 
+                            id='confirmPassword' 
+                            onChange={e => this.handleInputChange(e)}
+                            value={this.state.confirmPassword}
                             required 
                         />
                     </label>
+                    <span>{this.state.formErrors.confirmPassword}</span>
                     <div className="form-buttons">
                         <button type='button' onClick={this.props.handleCloseModal}>Cancel</button>
-                        <button type='submit'>Create Account</button>
+                        <button type='submit' disabled={!this.state.formValid}>Create Account</button>
                     </div>
                 </fieldset>
                 <p>Already have an account? <Link to='#' onClick={() => this.props.handleOpenModal('login')}>Log In</Link></p>
