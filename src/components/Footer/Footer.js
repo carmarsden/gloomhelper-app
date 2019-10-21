9a97c9e193fee7608297767b1d646ecd5417d94f
Caroline Marsden <car.marsden@gmail.com>
2019-10-21 10:14:28 -0400

    Add CC attribution info

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,13 @@ import './Footer.css';
 function Footer() {
     return (
         <footer role='contentinfo'>
-            Created by Caroline Marsden{'  '}
-            <a href='https://github.com/carmarsden' target='_blank' rel='noopener noreferrer'>GitHub</a>
+            <p>
+                <a href='https://boardgamegeek.com/thread/1733586/files-creation' target='_blank' rel='noopener noreferrer'>All Gloomhaven image assets</a> by <a href='https://www.cephalofair.com/isaac' target='_blank' rel='noopener noreferrer'>Isaac Childress</a> are licensed under <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/' target='_blank' rel='noopener noreferrer'>CC BY-NC-SA 4.0</a>.
+            </p>
+            <p>
+                Created by Caroline Marsden{'  '}
+                <a href='https://github.com/carmarsden' target='_blank' rel='noopener noreferrer'>GitHub</a>
+            </p>
         </footer>
     );    
 }
